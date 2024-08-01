4fdaff6caa6438843e886a75eac3f563abf275a9
João Gabriel Portela <jgportelaf@gmail.com>
2024-08-01 17:15:25 -0300

    Php - Versão 2

--- consulta.php
+++ consulta.php
@@ -1,16 +1,26 @@
 <?php
-include "conecta.php";
-$sql ="SELECT id, nome, senha, email FROM usuario";
-$resultado = mysqli_query($conexao, $sql);
-echo "<br>";
-if (mysqli_num_rows($resultado) > 0) {
-    while($registro = mysqli_fetch_assoc($resultado)){
-        echo "id: " . $registro["id"]. " - Nome: ".$registro["nome"]. "".$registro ["senha"]
-        . " - Email: ".$registro["email"]. "<br>";
-
-
+    include "conecta.php";
+    $sql = "SELECT id, nome, senha, email FROM usuario";
+    $resultado = mysqli_query($conexao, $sql);
+    echo "<br>";
+    if (mysqli_num_rows($resultado) > 0){
+        while ($registro = mysqli_fetch_assoc($resultado)){
+            echo "id: " . $registro["id"]. " nome: " . $registro["nome"].
+            " senha: ". $registro["senha"]. "email:". $registro["email"]. "<br>";
+        }
+    } else {
+        echo "Nenhum registro encontrado!";
     }
-} else {
-    echo "Nenhum registro encontrado"
-}
-?>
+    echo"<BR>Nomes que começam com 'c'<br>";
+    $sql = "SELECT id, nome, senha, email FROM usuario where nome like 'c%'";
+    $resultado = mysqli_query($conexao, $sql);
+    echo "<br>";
+    if (mysqli_num_rows($resultado) > 0){
+        while ($registro = mysqli_fetch_assoc($resultado)){
+            echo "id: " . $registro["id"]. " nome: " . $registro["nome"].
+            " senha: ". $registro["senha"]. "email:". $registro["email"]. "<br>";
+        }
+    } else {
+        echo "Nenhum registro encontrado!";
+    }
+    ?>
